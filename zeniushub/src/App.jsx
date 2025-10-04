9dbad2171611976ceb974f23ee78c21a8a8193d0
bhumi-choudhary <bhumichoudhary200@gmail.com>
2025-10-04 10:44:22 +0530

    created an folder setup

--- zeniushub/src/App.jsx
+++ zeniushub/src/App.jsx
@@ -1,43 +1,12 @@
-import React, { useRef, useEffect } from "react";
-import Button from "@mui/material/Button";
-import { gsap } from "gsap";
+import React from "react";
+import Header from "./layout/Header";
 
 const App = () => {
-  const btnRef = useRef(null);
-  const textRef = useRef(null);
 
-  useEffect(() => {
-    // Button simple fade + slide
-    gsap.from(btnRef.current, {
-      opacity: 0,
-      y: -30, // thoda upar se
-      duration: 1,
-      ease: "power2.out",
-    });
-
-    // Text simple fade + slide
-    gsap.from(textRef.current, {
-      opacity: 0,
-      x: -30, // left se
-      duration: 1,
-      delay: 0.5, // thoda baad me aaye
-      ease: "power2.out",
-    });
-  }, []);
 
   return (
-    <div style={{ padding: 20 }}>
-      <Button
-        ref={btnRef}
-        style={{ color: "black", backgroundColor: "pink", margin: 10 }}
-        variant="contained"
-      >
-        Hello world
-      </Button>
-
-      <div ref={textRef} className="text-red-500 m-6">
-        Tailwind downloaded successfully
-      </div>
+    <div >
+      <Header />
     </div>
   );
 };
